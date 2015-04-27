4b5a66383d1eb2268e6f40b7930ba7be270c604d
Adam McLean <adam.mclean1@gmail.com>
2015-04-27 13:19:11 -0700

    finish CRUD functionality with devise

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -8,11 +8,11 @@ class PinsController < ApplicationController
   end
 
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build #we get current_user from devise
   end
 
   def create
-    @pin = Pin.new(pin_params)
+    @pin = current_user.pins.build(pin_params)
 
     if @pin.save
       redirect_to pins_path, notice; 'Successfully created new Pin'
