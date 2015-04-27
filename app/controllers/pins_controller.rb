cb99a98ba9c71f33bf26aadaa24e456217d4f897
Adam McLean <adam.mclean1@gmail.com>
2015-04-27 11:07:06 -0700

    add RESTFUL route architecture

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,10 @@
 class PinsController < ApplicationController
+  before_action :find_pin, only: [:show, :edit, :update, :destroy]
   def index
-    @pin = Pin.all
+    @pins = Pin.all.order("created_at DESC")
+  end
+
+  def show
   end
 
   def new
@@ -11,10 +15,27 @@ class PinsController < ApplicationController
     @pin = Pin.new(pin_params)
 
     if @pin.save
-      redirect_to @pin, notice; 'Successfully created new Pin'
+      redirect_to pins_path, notice; 'Successfully created new Pin'
     else
       render 'new'
     end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: "Pin was successfully updated"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @pin.destroy #already finds pin from our before_action :find_pin
+    redirect_to root_path
   end
 
 
@@ -22,4 +43,9 @@ class PinsController < ApplicationController
   def pin_params
     params.require(:pin).permit(:title, :description)
   end
+
+  def find_pin
+    @pin = Pin.find(params[:id])
+  end
+
 end
