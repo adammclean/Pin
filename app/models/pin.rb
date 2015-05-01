6398cd758da85755cfe69ca1cfa87b068f392e0d
Adam McLean <adam.mclean1@gmail.com>
2015-05-01 14:10:14 -0700

    still debugging

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,5 @@ belongs_to :user
 acts_as_votable
 
 has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/end
+  # validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/end
+end
